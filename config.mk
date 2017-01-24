6bb3d821392809a232a0e39ab567ae5e7aa0c8d9
DirtyDragons <mkeller96@gmail.com>
2017-01-23 19:48:53 -0600

    Update config.mk

--- config.mk
+++ config.mk
@@ -7,24 +7,3 @@ PRODUCT_PACKAGES += \
     KernelAdiutor \
     SnapBrowser \
     Superuser \
-    NexusWallpapersStubPrebuilt \
-    WallpaperPickerGooglePrebuilt \
-    NexusLauncherPrebuilt \
-    WallpapersUsTwo
-
- # wallpaper location prop
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.wallpapers_loc_request_suw=true
-
-# live wallpapers arm64 only
-ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_PRODUCT)),)
-PRODUCT_PACKAGES += \
-    WallpapersBReel \
-    libgdx.so \
-    libgeswallpapers-jni.so \
-    libjpeg.so
-
-PRODUCT_COPY_FILES += \
-    vendor/addons/lib64/libgdx.so:system/lib64/libgdx.so \
-    vendor/addons/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
-endif
