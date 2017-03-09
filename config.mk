f3b472ba42e0d20bd2a7dbb8ae8fa9574d17422f
KillerDroid96 <mkeller96@gmail.com>
2017-03-08 21:32:46 -0600

    Updated Substratum + KA & added back SnapBrowser

--- config.mk
+++ config.mk
@@ -1,8 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
+# Remove warning from every time you open it
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.substratum.verified=true \
+
 # Prebuilts
 PRODUCT_PACKAGES += \
     AdAway \
     KernelAdiutor \
+    SnapBrowser \
     Substratum
--- system/app/Android.mk
+++ system/app/Android.mk
@@ -13,6 +13,21 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_PREBUILT_JNI_LIBS_arm := \
 	@lib/armeabi/libblank_webserver_exec.so \
 	@lib/armeabi/libtcpdump_exec.so
+include $(CLEAR_VARS)
+LOCAL_MODULE       := SnapBrowser
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_SRC_FILES    := SnapBrowser.apk
+LOCAL_CERTIFICATE  := PRESIGNED
+LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
+LOCAL_OVERRIDES_PACKAGES := Browser Browser2
+LOCAL_PREBUILT_JNI_LIBS_arm := \
+	@lib/armeabi-v7a/libc++_shared.so \
+	@lib/armeabi-v7a/libicui18n.cr.so \
+	@lib/armeabi-v7a/libicuuc.cr.so \
+	@lib/armeabi-v7a/libswecore.so \
+	@lib/armeabi-v7a/libswe.so \
+	@lib/armeabi-v7a/libswev8.so
 include $(BUILD_PREBUILT)
 
 # Install Substratum
