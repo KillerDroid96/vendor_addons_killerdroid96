72f9bea8c1260b9037d825390ccaff2e34da89b9
KillerDroid96 <mkeller96@gmail.com>
2017-04-21 14:22:44 -0500

    Revert "Add Pixel wallpapers"
    This reverts commit 557f911377ea2b0dddbe73fd248f2c2083943cf7.

--- config.mk
+++ config.mk
@@ -5,19 +5,6 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.substratum.verified=true \
 
-# live wallpapers arm64 only
-ifneq ($(filter angler bullhead marlin sailfish,$(TARGET_DEVICE)),)
-    PRODUCT_PACKAGES += \
-        WallpapersBReel \
-        libgdx.so \
-        libgeswallpapers-jni.so \
-        libjpeg.so
-
-    PRODUCT_COPY_FILES += \
-        vendor/addons/dragon/prebuilt/lib64/libgdx.so:system/lib64/libgdx.so \
-        vendor/addons/dragon/prebuilt/lib64/libgeswallpapers-jni.so:system/lib64/libgeswallpapers-jni.so
-endif
-
 # Prebuilts
 PRODUCT_PACKAGES += \
     AdAway \
@@ -25,8 +12,4 @@ PRODUCT_PACKAGES += \
     SnapBrowser \
     Substratum \
     Spectrum \
-    Turbo \
-    LiveWallpapersPicker \
-    NexusWallpapersStubPrebuilt \
-    WallpaperPickerGooglePrebuilt \
-    WallpapersUsTwo
+    Turbo
