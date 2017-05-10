f3e96520d8facde57cd76e9ca634914f8f9493be
KillerDroid96 <KillerDroid96@users.noreply.github.com>
2017-05-09 21:21:10 -0500

    Update Android.mk

--- system/app/Android.mk
+++ system/app/Android.mk
@@ -14,24 +14,6 @@ LOCAL_PREBUILT_JNI_LIBS_arm := \
 	@lib/armeabi/libtcpdump_exec.so
 include $(BUILD_PREBUILT)
 
-#SnapBrowser
-include $(CLEAR_VARS)
-LOCAL_MODULE       := SnapBrowser
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_SRC_FILES    := SnapBrowser.apk
-LOCAL_CERTIFICATE  := PRESIGNED
-LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
-LOCAL_OVERRIDES_PACKAGES := Browser Browser2
-LOCAL_PREBUILT_JNI_LIBS_arm := \
-	@lib/armeabi-v7a/libc++_shared.so \
-	@lib/armeabi-v7a/libicui18n.cr.so \
-	@lib/armeabi-v7a/libicuuc.cr.so \
-	@lib/armeabi-v7a/libswecore.so \
-	@lib/armeabi-v7a/libswe.so \
-	@lib/armeabi-v7a/libswev8.so
-include $(BUILD_PREBUILT)
-
 # Substratum
 include $(CLEAR_VARS)
 LOCAL_MODULE := Substratum
@@ -50,15 +32,5 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := Spectrum.apk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := Turbo
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := Turbo.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
