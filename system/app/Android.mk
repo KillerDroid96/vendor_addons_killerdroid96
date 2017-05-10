5a8bb6a04546af847da1de71dd5b746d564aaa2e
KillerDroid96 <KillerDroid96@users.noreply.github.com>
2017-05-09 21:21:55 -0500

    Update Android.mk

--- system/app/Android.mk
+++ system/app/Android.mk
@@ -1,19 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-#AdAway
-include $(CLEAR_VARS)
-LOCAL_MODULE := AdAway
-LOCAL_SRC_FILES := AdAway.apk
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_MODULE_CLASS := APPS
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_PATH  := $(TARGET_OUT_APPS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PREBUILT_JNI_LIBS_arm := \
-	@lib/armeabi/libblank_webserver_exec.so \
-	@lib/armeabi/libtcpdump_exec.so
-include $(BUILD_PREBUILT)
-
 # Substratum
 include $(CLEAR_VARS)
 LOCAL_MODULE := Substratum
